46521a752eab0355f50ba6f435dcb029d002c113
Yao Yue <yao@twitter.com>
2014-03-29 17:45:42 -0700

    event module: temp

---
+++ src/event/cc_event.h
@@ -0,0 +1,76 @@
+/*
+ * ccommon - a cache common library.
+ * Copyright (C) 2013 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _CC_EVENT_H_
+#define _CC_EVENT_H_
+
+#include <cc_conn.h>
+
+#define EVENT_SIZE  1024
+
+#define EVENT_READ  0x0000ff
+#define EVENT_WRITE 0x00ff00
+#define EVENT_ERR   0xff0000
+
+typedef int (*event_cb_t)(void *, uint32_t);  /* event callback */
+
+#ifdef NC_HAVE_EPOLL
+
+struct event_base {
+    int                ep;      /* epoll descriptor */
+
+    struct epoll_event *event;  /* event[] - events that were triggered */
+    int                nevent;  /* # events */
+
+    event_cb_t         cb;      /* event callback */
+};
+
+#elif NC_HAVE_KQUEUE
+
+struct event_base {
+    int           kq;           /* kernel event queue descriptor */
+
+    struct kevent *change;      /* change[] - events we want to monitor */
+    int           nchange;      /* # change */
+
+    struct kevent *event;       /* event[] - events that were triggered */
+    int           nevent;       /* # events */
+    int           nreturned;    /* # events placed in event[] */
+    int           nprocessed;   /* # events processed from event[] */
+
+    event_cb_t    cb;           /* event callback */
+};
+
+#else
+# error missing scalable I/O event notification mechanism
+#endif
+
+struct event_base *event_base_create(int size, event_cb_t cb);
+void event_base_destroy(struct event_base *evb);
+
+/* NIO event APIs */
+int event_add_ni(struct event_base *evb, struct conn *c); /* network in */
+int event_del_ni(struct event_base *evb, struct conn *c);
+int event_add_no(struct event_base *evb, struct conn *c); /* network out */
+int event_del_no(struct event_base *evb, struct conn *c);
+int event_add_nc(struct event_base *evb, struct conn *c); /* network conn */
+int event_del_nc(struct event_base *evb, struct conn *c);
+
+/* timed event APIs */
+int event_time_wait(struct event_base *evb, int timeout);
+
+#endif /* _CC_EVENT_H */
