9a515186543dd6765f0c530543be6031dc8a3082
Kevin Yang <kyang@twitter.com>
2014-08-21 20:12:06 -0700

    refactored settings

--- src/cc_settings.c
+++ src/cc_settings.c
@@ -21,187 +21,92 @@
 #include <cc_define.h>
 #include <cc_log.h>
 #include <cc_mm.h>
-#include <cc_sds.h>
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
-
-#define SETTING_LINE_MAX 1024
-
-/* Sample Config File */
-/*
-  prealloc 1
-  evict_lru 0
-  use_freeq 1
-  use_cas 1
-  maxbytes 4294967296
-  slab_size 1050000
-  # Hash power not included, it is not required
-  profile 128 256 512 1024 2048 4096 8192 16387 32768 65536 131072 262144 524288 1048576
-  profile_last_id 14
-  oldest_live 6000
- */
-
-/* Whether or not settings have been initialized for the first time */
-static bool settings_initialized = false;
-
-
-
-/* Enum used to match setting to type in order to set values */
-typedef enum settings_type {
-    bool_val_setting,
-    uint8_val_setting,
-    uint32_val_setting,
-    uint64_val_setting,
-    reltime_val_setting,
-    uint32ptr_val_setting
-} settings_type_t;
-
-
-
-/* Initialize settings members */
-#define SETTINGS_INIT(_name, _required, _type, _dynamic, _default, _description) \
-    ._name = {.initialized = false, .val._type = _default},
-
-struct settings settings = { SETTINGS_MEM(SETTINGS_INIT) };
-
-
-
-/* Macro for checking whether or not all required settings have been initialized */
-#define SETTINGS_REQUIRED(_name, _required, _type, _dynamic, _default, _description) \
-    (!_required || settings._name.initialized) &&
-
-
-/* Macro for carrying out config file directives */
-#define SETTINGS_EXE(_name, _required, _type, _dynamic, _default, _description)    \
-    if(!strcasecmp(argv[0], #_name) && argc >= 2) {                                \
-        if(_dynamic && settings_initialized) {                                     \
-	    log_debug(LOG_NOTICE, "Cannot overwrite non dynamic setting " #_name); \
-	} else {                                                                   \
-	    set_setting(&settings._name, _type ## _setting, argc, argv);	   \
-	    settings._name.initialized = true;                                     \
-	}                                                                          \
-    } else                                                                         \
-
-static void set_setting(struct setting *setting,  settings_type_t type, int32_t argc, sds *argv);
 
 rstatus_t
-settings_load(char *config_file)
+settings_set(struct setting *setting, struct setting_val *val)
 {
-    FILE *fp;
-    char buf[SETTING_LINE_MAX];
-    uint32_t linenum = 0;
-    sds line = NULL;
-
-    if(config_file[0] == '-' && config_file[1] == '\0') {
-	fp = stdin;
-    } else {
-	if((fp = fopen(config_file, "r")) == NULL) {
-	    log_debug(LOG_CRIT, "Could not open config file %s!", config_file);
-	    return CC_ERROR;
-	}
+    switch(val->type) {
+    case bool_val_setting:
+	setting->val.bool_val = *((bool *)val->val);
+	setting->initialized = true;
+	cc_free(val->val);
+	break;
+    case uint8_val_setting:
+	setting->val.uint8_val = *((uint8_t *)val->val);
+	setting->initialized = true;
+	cc_free(val->val);
+	break;
+    case uint32_val_setting:
+	setting->val.uint32_val = *((uint32_t *)val->val);
+	setting->initialized = true;
+	cc_free(val->val);
+	break;
+    case uint64_val_setting:
+	setting->val.uint64_val = *((uint64_t *)val->val);
+	setting->initialized = true;
+	cc_free(val->val);
+	break;
+    case reltime_val_setting:
+	setting->val.reltime_val = *((rel_time_t *)val->val);
+	setting->initialized = true;
+	cc_free(val->val);
+	break;
+    case uint32ptr_val_setting: {
+	setting->val.uint32ptr_val = val->val;
+	setting->initialized = true;
+	break;
     }
-
-    while(fgets(buf, SETTING_LINE_MAX + 1, fp) != NULL) {
-	sds *argv;
-	int32_t argc;
-	uint32_t i;
-
-	++linenum;
-	line = sdsnew(buf);
-	line = sdstrim(line, " \t\r\n");
-
-	/* Skip comments and blank lines */
-	if(line[0] == '#' || line[0] == '\0') {
-	    sdsfree(line);
-	    continue;
-	}
-
-	/* Split into arguments */
-	argv = sdssplitargs(line, &argc);
-	sdstolower(argv[0]);
-
-	/* execute config file directives */
-	SETTINGS_MEM(SETTINGS_EXE)
-	{
-	    log_debug(LOG_CRIT, "Error in config file: incorrect number or type of elements at line %u", linenum);
-	    log_debug(LOG_CRIT, ">>> '%s'", line);
-
-	    for(i = 0; i < argc; ++i) {
-		sdsfree(argv[i]);
-	    }
-	    cc_free(argv);
-	    sdsfree(line);
-
-	    return CC_ERROR;
-	}
-
-	for(i = 0; i < argc; ++i) {
-	    sdsfree(argv[i]);
-	}
-	cc_free(argv);
-	sdsfree(line);
-    }
-
-    if(fp != stdin) {
-	fclose(fp);
-    }
-
-    if(!settings_initialized) {
-	if(!(SETTINGS_MEM(SETTINGS_REQUIRED)1)) {
-	    /* Not all required settings were initialized */
-	    log_debug(LOG_CRIT, "Error in config file: some required options were not initialized");
-	    return CC_ERROR;
-	}
-	settings_initialized = true;
+    default:
+	log_debug(LOG_CRIT, "fatal: unrecognized setting type");
+	return CC_ERROR;
     }
 
     return CC_OK;
 }
 
-/* Print out a description of each setting */
-#define SETTINGS_PRINT(_name, _required, _type, _dynamic, _default, _description) \
-    loga(#_name ": %s", _description);
-
-void
-settings_desc(void)
+void *
+settings_str_to_val(settings_type_t type, int32_t argc, sds *argv)
 {
-    SETTINGS_MEM(SETTINGS_PRINT)
-}
-
-/* Sets the given setting, depending on the type */
-static void
-set_setting(struct setting *setting, settings_type_t type, int32_t argc, sds *argv)
-{
-    ASSERT(type != NULL);
+    ASSERT(argc >= 2);
+    void *ret;
 
     switch(type) {
     case bool_val_setting:
-	setting->val.bool_val = atoi(argv[1]);
+	ret = cc_alloc(sizeof(bool));
+	*((bool *)ret) = atoi(argv[1]);
 	break;
     case uint8_val_setting:
-	setting->val.uint8_val = atoi(argv[1]);
+	ret = cc_alloc(sizeof(uint8_t));
+	*((uint8_t *)ret) = atoi(argv[1]);
 	break;
     case uint32_val_setting:
-	setting->val.uint32_val = atoi(argv[1]);
+	ret = cc_alloc(sizeof(uint32_t));
+	*((uint32_t *)ret) = atoi(argv[1]);
 	break;
     case uint64_val_setting:
-	setting->val.uint64_val = atol(argv[1]);
+	ret = cc_alloc(sizeof(uint64_t));
+	*((uint64_t *)ret) = atol(argv[1]);
 	break;
     case reltime_val_setting:
-	setting->val.reltime_val = atoi(argv[1]);
+	ret = cc_alloc(sizeof(rel_time_t));
+	*((rel_time_t *)ret) = atoi(argv[1]);
 	break;
     case uint32ptr_val_setting: {
 	uint32_t i;
-	setting->val.uint32ptr_val = cc_alloc((argc - 1) * sizeof(uint32_t));
+
+	ret = cc_alloc((argc - 1) * sizeof(uint32_t));
 	for(i = 0; i < argc - 1; ++i) {
-	    setting->val.uint32ptr_val[i] = atoi(argv[i + 1]);
+	    ((uint32_t *)ret)[i] = atoi(argv[i + 1]);
 	}
 	break;
     }
     default:
-	log_debug(LOG_CRIT, "fatal: unrecognized setting type");
-	exit(1);
+	log_debug(LOG_CRIT, "unknown setting type");
+	ret = NULL;
+	break;
     }
+
+    return ret;
 }
