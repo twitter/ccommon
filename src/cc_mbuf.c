82cc9d0f65b1a1a665a3f8336f09a12d980f8260
Yao Yue <yao@twitter.com>
2014-12-08 14:29:26 -0800

    log_info for setup

--- src/cc_mbuf.c
+++ src/cc_mbuf.c
@@ -343,7 +343,7 @@ mbuf_setup(uint32_t chunk_size)
     mbuf_init = true;
     ASSERT(mbuf_offset > 0 && mbuf_offset < mbuf_chunk_size);
 
-    log_debug("mbuf: chunk size %zu, hdr size %d, offset %zu",
+    log_info("mbuf: chunk size %zu, hdr size %d, offset %zu",
               mbuf_chunk_size, MBUF_HDR_SIZE, mbuf_offset);
 }
 
