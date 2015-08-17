8c455ad2242aea27a56cf7bea7f68c2500ff0f09
Kevin Yang <kyang@twitter.com>
2015-08-17 16:44:55 -0700

    ccommon: fix bugs for large values
    RB_ID=728020

--- src/stream/cc_sockio.c
+++ src/stream/cc_sockio.c
@@ -151,7 +151,6 @@ dbuf_tcp_read(struct buf_sock *s)
     ASSERT(c != NULL && h != NULL && s->rbuf != NULL);
     ASSERT(h->recv != NULL);
 
-
     do {
         /*
          * Try to recv:
@@ -165,7 +164,7 @@ dbuf_tcp_read(struct buf_sock *s)
          */
         cap = buf_wsize(s->rbuf);
         if (cap == 0) {
-            status = dbuf_double(&s->rbuf);
+            status = dbuf_double(&(s->rbuf));
             if (status != CC_OK) {
                 log_verb("doubling rbuf on buf_sock %p failed: %d", s, status);
                 status = CC_ERETRY;
