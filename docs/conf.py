6eb1bd9ed8af68c82b0acc25bdaf5e0332e6350f
Yao Yue <yao@twitter.com>
2015-09-19 17:27:17 -0700

    add logo

--- docs/conf.py
+++ docs/conf.py
@@ -122,16 +122,13 @@ html_theme = "default"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = u'_static/img/white_pelican.jpg'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
 #html_favicon = None
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
