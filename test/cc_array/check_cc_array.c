23ed5f78b45a741a334b760ef32f08436097c6a4
Sebastian Waisbrot <swaisbrot@twitter.com>
2015-11-06 22:44:56 -0800

    cc_array test set up

---
+++ test/cc_array/check_cc_array.c
@@ -0,0 +1,62 @@
+#include <cc_array.h>
+
+#include <check.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define SUITE_NAME "cc_array"
+#define DEBUG_LOG  SUITE_NAME ".log"
+
+/*
+ * utilities
+ */
+static void
+test_setup(void)
+{
+}
+
+static void
+test_reset(void)
+{
+}
+
+static void
+test_teardown(void)
+{
+}
+
+/*
+ * test suite
+ */
+static Suite *
+cc_array_suite(void)
+{
+    Suite *s = suite_create(SUITE_NAME);
+
+    return s;
+}
+/**************
+ * test cases *
+ **************/
+
+int
+main(void)
+{
+    int nfail;
+
+    /* setup */
+    test_setup();
+
+    Suite *suite = cc_array_suite();
+    SRunner *srunner = srunner_create(suite);
+    srunner_set_log(srunner, DEBUG_LOG);
+    srunner_run_all(srunner, CK_ENV); /* set CK_VEBOSITY in ENV to customize */
+    nfail = srunner_ntests_failed(srunner);
+    srunner_free(srunner);
+
+    /* teardown */
+    test_teardown();
+
+    return (nfail == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
