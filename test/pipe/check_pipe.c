cac962c7c54bebd05c9fe8aae25401692fdb9254
Sebastian Waisbrot <swaisbrot@twitter.com>
2015-11-27 14:22:11 -0800

    Add pipe read blocking test

--- test/pipe/check_pipe.c
+++ test/pipe/check_pipe.c
@@ -1,12 +1,22 @@
 #include <channel/cc_pipe.h>
+#include <time/cc_timer.h>
 
 #include <check.h>
 
+#include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #define SUITE_NAME "pipe"
 #define DEBUG_LOG  SUITE_NAME ".log"
+
+struct write_task {
+    struct pipe_conn *pipe;
+    void *buf;
+    size_t nbytes;
+    useconds_t usleep;
+};
 
 /*
  * utilities
@@ -30,10 +40,21 @@ test_reset(void)
     test_setup();
 }
 
+static void *do_write(void *_write_task)
+{
+    struct write_task* task = _write_task;
+    if (task->usleep) {
+        usleep(task->usleep);
+    }
+    ck_assert_int_eq(pipe_send(task->pipe, task->buf, task->nbytes), task->nbytes);
+    return NULL;
+}
+
 START_TEST(test_send_recv)
 {
     struct pipe_conn *pipe;
     const char *write_message = "foo bar baz";
+    struct write_task task;
 #define READ_MESSAGE_LENGTH 12
     char read_message[READ_MESSAGE_LENGTH];
     test_reset();
@@ -42,7 +63,12 @@ START_TEST(test_send_recv)
     ck_assert_ptr_ne(pipe, NULL);
 
     ck_assert_int_eq(pipe_open(NULL, pipe), true);
-    ck_assert_int_eq(pipe_send(pipe, (void *)write_message, READ_MESSAGE_LENGTH), READ_MESSAGE_LENGTH);
+
+    task.pipe = pipe;
+    task.buf = (void *)write_message;
+    task.nbytes = READ_MESSAGE_LENGTH;
+    task.usleep = 0;
+    do_write(&task);
 
     ck_assert_int_eq(pipe_recv(pipe, read_message, READ_MESSAGE_LENGTH), READ_MESSAGE_LENGTH);
 
@@ -54,6 +80,49 @@ START_TEST(test_send_recv)
 }
 END_TEST
 
+START_TEST(test_read_blocking)
+{
+    struct pipe_conn *pipe;
+    const char *write_message = "foo bar baz";
+    struct duration duration;
+    struct write_task task;
+    pthread_t thread;
+#define READ_MESSAGE_LENGTH 12
+#define SLEEP_TIME 500000
+#define TOLERANCE_TIME 100000
+    char read_message[READ_MESSAGE_LENGTH];
+    test_reset();
+    duration_reset(&duration);
+
+    pipe = pipe_conn_create();
+    ck_assert_ptr_ne(pipe, NULL);
+
+    ck_assert_int_eq(pipe_open(NULL, pipe), true);
+
+    duration_start(&duration);
+
+    task.pipe = pipe;
+    task.buf = (void *)write_message;
+    task.nbytes = READ_MESSAGE_LENGTH;
+    task.usleep = SLEEP_TIME;
+    pthread_create(&thread, NULL, do_write, &task);
+    ck_assert_int_eq(pipe_recv(pipe, read_message, READ_MESSAGE_LENGTH), READ_MESSAGE_LENGTH);
+
+    duration_stop(&duration);
+    pthread_join(thread, NULL);
+
+    ck_assert_int_ge(duration_us(&duration), SLEEP_TIME);
+    ck_assert_int_le(duration_us(&duration), SLEEP_TIME + TOLERANCE_TIME);
+
+    ck_assert_str_eq(write_message, read_message);
+
+    pipe_close(pipe);
+    pipe_conn_destroy(&pipe);
+#undef READ_MESSAGE_LENGTH
+#undef SLEEP_TIME
+#undef TOLERANCE_TIME
+}
+END_TEST
 /*
  * test suite
  */
@@ -64,6 +133,7 @@ pipe_suite(void)
 
     TCase *tc_pipe = tcase_create("pipe test");
     tcase_add_test(tc_pipe, test_send_recv);
+    tcase_add_test(tc_pipe, test_read_blocking);
     suite_add_tcase(s, tc_pipe);
 
     return s;
