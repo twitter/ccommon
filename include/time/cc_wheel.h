b3b13eab816c4d2af6b47a03d99052b812139cf7
Yao Yue <yao@twitter.com>
2015-12-01 22:27:06 -0800

    small updates fixing some bugs

--- include/time/cc_wheel.h
+++ include/time/cc_wheel.h
@@ -110,11 +110,11 @@ struct timing_wheel {
 };
 
 
-void timeout_event_reset(struct timeout_event *tev);
+void timeout_event_reset(struct timeout_event *t);
 struct timeout_event *timeout_event_create(void);
-void timeout_event_destroy(struct timeout_event **tev);
+void timeout_event_destroy(struct timeout_event **t);
 struct timeout_event *timeout_event_borrow(void);
-void timeout_event_return(struct timeout_event **tev);
+void timeout_event_return(struct timeout_event **t);
 
 void timeout_event_pool_create(uint32_t max);
 void timeout_event_pool_destroy(void);
