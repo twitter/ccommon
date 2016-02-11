eb2c2483714566f64d63e15ec28cdf946d9ad224
Yao Yue <yao@twitter.com>
2016-02-10 16:18:19 -0800

    refactor ring buffer module

--- include/cc_log.h
+++ include/cc_log.h
@@ -27,13 +27,12 @@ extern "C" {
 
 #include <stdbool.h>
 
-#define LOG_MAX_LEN 2560 /* max length of log message */
+#define LOG_MAX_LEN 2560 /* max length of log message to STDOUT/STDERR */
 
 struct logger {
     char *name;                 /* log file name */
     int  fd;                    /* log file descriptor */
-    int  nerror;                /* # log error */
-    struct rbuf *buf;           /* buffer for pausless logging */
+    struct rbuf *buf;           /* ring buffer for pauseless logging */
 };
 
 /*          name            type            description */
