1545cac6d4b8697a5abe0ef41bff14c31b1d90c7
Yao Yue <yao@twitter.com>
2016-03-07 15:45:27 -0800

    bug fix on return value

--- src/cc_log.c
+++ src/cc_log.c
@@ -282,13 +282,13 @@ log_flush(struct logger *logger)
     size_t buf_len;
 
     if (logger->buf == NULL) {
-        return;
+        return 0;
     }
 
     if (logger->fd < 0) {
         log_stderr("Cannot flush logger %p; invalid file descriptor", logger);
         INCR(log_metrics, log_flush_ex);
-        return;
+        return 0;
     }
 
     buf_len = rbuf_rcap(logger->buf);
