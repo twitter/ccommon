9aad05dfb5504b18beda8cedae978a96e9fe9f8d
Yao Yue <yao@twitter.com>
2016-03-14 03:20:14 -0700

    small change to debug setup

--- include/cc_debug.h
+++ include/cc_debug.h
@@ -27,16 +27,16 @@ extern "C" {
 
 #include <stdint.h>
 
-#define DEBUG_LOG_LEVEL 4         /* default log level */
-#define DEBUG_LOG_NBUF  4 * MiB   /* default log buf size */
-#define DEBUG_LOG_INTVL 100000000 /* flush every 100 milliseconds */
+#define DEBUG_LOG_LEVEL 4       /* default log level */
+#define DEBUG_LOG_NBUF  4 * MiB /* default log buf size */
+#define DEBUG_LOG_INTVL 100     /* flush every 100 milliseconds */
 
 /*          name             type              default           description */
 #define DEBUG_OPTION(ACTION)                                                                                                      \
     ACTION( debug_log_level, OPTION_TYPE_UINT, DEBUG_LOG_LEVEL,  "debug log level"                                               )\
     ACTION( debug_log_file,  OPTION_TYPE_STR,  NULL,             "debug log file"                                                )\
     ACTION( debug_log_nbuf,  OPTION_TYPE_UINT, DEBUG_LOG_NBUF,   "debug log buf size"                                            )\
-    ACTION( debug_log_intvl, OPTION_TYPE_UINT, DEBUG_LOG_INTVL,  "debug log flush interval in ns (only applies if buf size > 0)")
+    ACTION( debug_log_intvl, OPTION_TYPE_UINT, DEBUG_LOG_INTVL,  "debug log flush interval in ms (only applies if buf size > 0)")
 
 typedef struct {
     DEBUG_OPTION(OPTION_DECLARE)
