d883d33454622d6f71b401b60468e1065f06739f
Jonathan Simms <jsimms@twitter.com>
2018-07-06 19:22:24 -0400

    add some docs and tests

--- rust/ccommon/src/bstring.rs
+++ rust/ccommon/src/bstring.rs
@@ -1,15 +1,59 @@
 use cc_binding as bind;
-
-use std::slice;
+use std::boxed::Box;
+use std::convert::AsMut;
+use std::ffi::CString;
 use std::io;
 use std::ops::{Deref, DerefMut};
 use std::os::raw::c_char;
-use std::convert::AsMut;
+use std::slice;
+
+/// A wrapper around a string reference that returns a properly initialized `*mut bstring` pointer.
+/// Useful for testing and possibly in other circumstances.
+///
+pub struct BStringStr<'a>(pub &'a str);
+
+impl<'a> BStringStr<'a> {
+    #[allow(dead_code)]
+    pub fn into_raw(self) -> *mut bind::bstring {
+        let bs = bind::bstring{
+            len: self.0.len() as u32,
+            data: CString::new(self.0).unwrap().into_raw(),
+        };
+
+        Box::into_raw(Box::new(bs))
+    }
+
+    /// Frees a BStringStr that was previously converted into a `*mut bstring` via the
+    /// into_raw method. Passing this method a pointer created through other means
+    /// may lead to undefined behavior.
+    #[allow(dead_code)]
+    pub unsafe fn free(ptr: *mut bind::bstring) {
+        let b: Box<bind::bstring> = Box::from_raw(ptr);
+        // reclaim pointer from the bstring, allowing it to be freed
+        drop(CString::from_raw(b.data));
+        drop(b);
+    }
+}
+
 
 /// BStringRef provides a wrapper around a raw pointer to a cc_bstring. It's important to note that
 /// this struct does not take ownership of the underlying pointer, nor does it free it when it's
 /// dropped.
 ///
+/// # Examples
+/// ```rust
+///     use ccommon::bstring::{BStringStr, BStringRef};
+///
+///     let s = "sea change";
+///     let bsp = BStringStr(s).into_raw();
+///     let bsr = unsafe { BStringRef::from_raw(bsp) };
+///
+///     assert_eq!(&bsr[0..4], b"sea ");
+///     assert_eq!(&bsr[0..10], b"sea change");
+///     assert_eq!(&bsr[..], b"sea change");
+///
+///     unsafe { BStringStr::free(bsp) };
+/// ```
 // see go/rust-newtype-pattern
 pub struct BStringRef<'a> {
     inner: &'a bind::bstring,
@@ -41,7 +85,33 @@ impl<'a> Deref for BStringRef<'a> {
     }
 }
 
-
+/// BStringRef provides a wrapper around a raw pointer to a cc_bstring. It's important to note that
+/// this struct does not take ownership of the underlying pointer, nor does it free it when it's
+/// dropped.
+///
+/// # Examples
+/// ```rust
+/// # use ccommon::bstring::{BStringStr, BStringRefMut};
+///
+/// use std::io::Write;
+///
+/// let s = "sea change";
+/// let bsp = BStringStr(s).into_raw();
+/// let mut bsr = unsafe { BStringRefMut::from_raw(bsp) };
+///
+/// let d = vec![0u8, 1u8, 2u8];
+/// assert_eq!(d.len(), 3);
+///
+/// {
+///     let mut buf: &mut [u8] = &mut bsr;
+///     let n = buf.write(&d).unwrap();
+///     assert_eq!(n, 3);
+/// }
+///
+/// assert_eq!(&bsr[0..3], &d[0..3]);
+///
+/// unsafe { BStringStr::free(bsp) };
+/// ```
 #[derive(Debug)]
 pub struct BStringRefMut<'a> {
     inner: &'a mut bind::bstring,
@@ -98,67 +168,3 @@ impl<'a> AsMut<bind::bstring> for BStringRefMut<'a> {
     }
 }
 
-
-#[cfg(test)]
-mod test {
-    use super::*;
-    use std::ffi::CString;
-    use std::boxed::Box;
-    use std::io::Write;
-
-    struct BStringStr<'a>(&'a str);
-
-    impl<'a> BStringStr<'a> {
-        fn into_raw(self) -> *mut bind::bstring {
-            let bs = bind::bstring{
-                len: self.0.len() as u32,
-                data: CString::new(self.0).unwrap().into_raw(),
-            };
-
-            Box::into_raw(Box::new(bs))
-        }
-
-        /// Frees a BStringStr that was previously converted into a *mut bind::bstring via the
-        /// into_raw method. Passing this method a pointer created through other means
-        /// may lead to undefined behavior.
-        unsafe fn free(ptr: *mut bind::bstring) {
-            let b: Box<bind::bstring> = Box::from_raw(ptr);
-            // reclaim pointer from the bstring, allowing it to be freed
-            let _x = CString::from_raw(b.data);
-        }
-    }
-
-    #[test]
-    fn bstring_ref() {
-        let s = "sea change";
-        let bsp = BStringStr(s).into_raw();
-        let bsr = unsafe { BStringRef::from_raw(bsp) };
-
-        assert_eq!(&bsr[0..4], b"sea ");
-        assert_eq!(&bsr[0..10], b"sea change");
-        assert_eq!(&bsr[..], b"sea change");
-
-        unsafe { BStringStr::free(bsp) };
-    }
-
-    #[test]
-    fn bstring_ref_mut() {
-        let s = "sea change";
-        let bsp = BStringStr(s).into_raw();
-        let mut bsr = unsafe { BStringRefMut::from_raw(bsp) };
-
-        let d = vec![0u8, 1u8, 2u8];
-        assert_eq!(d.len(), 3);
-
-        {
-            let mut buf: &mut [u8] = &mut bsr;
-            let n = buf.write(&d).unwrap();
-            assert_eq!(n, 3);
-        }
-        
-
-        assert_eq!(&bsr[0..3], &d[0..3]);
-
-        unsafe { BStringStr::free(bsp) };
-    }
-}
