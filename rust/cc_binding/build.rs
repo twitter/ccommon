6b59401474b9035e769cafe8c46a492989f09fd0
Jonathan Simms <xyqzku@gmail.com>
2018-07-19 22:00:20 -0400

    Rust build tweak (#165)
    part of the integration work of merging ccommon back into pelikan
    * fix how build.rs finds the ccommon lib
    * a few additional properties for BStr (needed by CDB)

--- rust/cc_binding/build.rs
+++ rust/cc_binding/build.rs
@@ -1,11 +1,19 @@
 extern crate bindgen;
+#[macro_use]
+extern crate failure;
 
 use std::env;
 use std::fs;
 use std::io;
-use std::path::PathBuf;
+use std::io::BufReader;
 use std::io::prelude::*;
+use std::ffi::OsString;
 use std::os::unix::fs as unix_fs;
+use std::path::Path;
+use std::path::PathBuf;
+use std::result;
+
+type Result<T> = result::Result<T, failure::Error>;
 
 fn get_cmake_binary_dir() -> io::Result<String> {
     // this file is written by cmake on each run, updated with the location of
@@ -15,6 +23,28 @@ fn get_cmake_binary_dir() -> io::Result<String> {
     let n = fp.read_to_string(&mut buf)?;
     assert!(n > 0, "file was empty");
     Ok(String::from(buf.trim_right()))
+}
+
+const CMAKE_CACHE: &str = "CMakeCache.txt";
+const CCOMMON_BINARY_DIR_KEY: &str = "ccommon_BINARY_DIR:STATIC";
+
+fn get_cmake_cache_value(binary_dir: &Path, key: &str) -> Result<Option<String>> {
+    let cache_path = binary_dir.join(CMAKE_CACHE);
+    let fp = BufReader::new(fs::File::open(cache_path)?);
+
+    for x in fp.lines() {
+        let line = x?;
+        let needle = format!("{}=", key);
+        if line.starts_with(&needle[..]) {
+            if let Some(v) = line.rsplit("=").take(1).last() {
+                return Ok(Some(v.to_owned()))
+            } else {
+                bail!("bad line: {:#?}", line);
+            }
+        }
+    }
+
+    Ok(None)
 }
 
 fn main() {
@@ -32,17 +62,26 @@ fn main() {
     let mut config_h_dir = cbd.clone();
     config_h_dir.push("ccommon");
 
-    let mut lib_dir = cbd.clone();
-    lib_dir.push("lib");
+    let lib_dir: String = {
+        let cbd = get_cmake_cache_value(&cbd, CCOMMON_BINARY_DIR_KEY)
+            .map(|o| o.map(OsString::from))
+            .unwrap()
+            .expect(
+                format!("could not find {} in {}", CCOMMON_BINARY_DIR_KEY, CMAKE_CACHE).as_ref()
+            );
 
-    println!("cargo:rustc-link-search=native={}", lib_dir.to_str().unwrap());
+        let cbd = Path::new(&cbd);
+        cbd.join("lib").to_str().unwrap().to_string()
+    };
+
+    println!("cargo:rustc-link-search=native={}", lib_dir);
 
     let bindings = bindgen::Builder::default()
         .clang_args(vec![
             "-I", include_path.to_str().unwrap(),
             "-I", config_h_dir.to_str().unwrap(),
             "-I", cbd.to_str().unwrap(),
-            "-L", lib_dir.to_str().unwrap(),
+            "-L", &lib_dir,
         ])
         .header("wrapper.h")
         .blacklist_type("max_align_t") // https://github.com/rust-lang-nursery/rust-bindgen/issues/550
--- rust/ccommon_rs/src/bstring.rs
+++ rust/ccommon_rs/src/bstring.rs
@@ -33,6 +33,7 @@ use std::mem;
 use std::ops::{Deref, DerefMut};
 use std::slice;
 use std::str;
+use std::borrow::BorrowMut;
 
 
 pub type CCbstring = bind::bstring;
@@ -87,7 +88,7 @@ impl BStr {
     }
 
     #[inline]
-    fn as_ptr(&self) -> *mut CCbstring {
+    pub fn as_ptr(&self) -> *mut CCbstring {
         self as *const _ as *mut _
     }
 }
@@ -105,6 +106,30 @@ impl DerefMut for BStr {
     #[inline]
     fn deref_mut(&mut self) -> &mut [u8] {
         unsafe { raw_ptr_to_bytes_mut(self.as_ptr()) }
+    }
+}
+
+impl AsRef<CCbstring> for BStr {
+    fn as_ref(&self) -> &CCbstring {
+        unsafe { &*self.as_ptr() }
+    }
+}
+
+impl AsMut<CCbstring> for BStr {
+    fn as_mut(&mut self) -> &mut CCbstring {
+        unsafe { &mut *(self.as_ptr() as *mut _)}
+    }
+}
+
+impl Borrow<CCbstring> for BStr {
+    fn borrow(&self) -> &CCbstring {
+        unsafe { &*self.as_ptr() }
+    }
+}
+
+impl BorrowMut<CCbstring> for BStr {
+    fn borrow_mut(&mut self) -> &mut CCbstring {
+        unsafe { &mut *(self.as_ptr() as *mut _)}
     }
 }
 
@@ -239,6 +264,7 @@ impl BString {
     ///
     /// This method will panic if `src.len() != self.len()`
     #[inline]
+    #[allow(dead_code)]
     fn copy_from_slice(&mut self, src: &[u8]) {
         assert_eq!(src.len(), self.len());
         (&mut (**self)).copy_from_slice(&src[..]);
@@ -446,7 +472,7 @@ mod test {
         {
             let mut c = Cursor::new(&mut x[..]);
             let f = "fantastic".as_bytes();
-            c.seek(SeekFrom::End(-9));
+            c.seek(SeekFrom::End(-9)).unwrap();
 
             let sz = c.write(&f[..]).unwrap();
             assert_eq!(sz, f.len());
