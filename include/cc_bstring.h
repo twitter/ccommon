3de230c12d3a69849ef0d5f8e889a3cc01e58729
Yao Yue <yao@twitter.com>
2018-08-16 16:16:02 -0700

    remove endian-specific logic from str*cmp

--- include/cc_bstring.h
+++ include/cc_bstring.h
@@ -58,7 +58,6 @@ int bstring_compare(const struct bstring *s1, const struct bstring *s2);
 struct bstring *bstring_alloc(uint32_t size);
 void bstring_free(struct bstring **bstring);
 
-/* TODO(yao): is this endian thing really useful? */
 /* efficient implementation of string comparion of short strings */
 #define str2cmp(m, c0, c1)                                                     \
     (m[0] == c0 && m[1] == c1)
@@ -66,43 +65,8 @@ void bstring_free(struct bstring **bstring);
 #define str3cmp(m, c0, c1, c2)                                                 \
     (m[0] == c0 && m[1] == c1 && m[2] == c2)
 
-#ifdef CC_LITTLE_ENDIAN
-
 #define str4cmp(m, c0, c1, c2, c3)                                             \
-    (*(uint32_t *) m == ((c3 << 24) | (c2 << 16) | (c1 << 8) | c0))
-
-#define str5cmp(m, c0, c1, c2, c3, c4)                                         \
-    (str4cmp(m, c0, c1, c2, c3) && (m[4] == c4))
-
-#define str6cmp(m, c0, c1, c2, c3, c4, c5)                                     \
-    (str4cmp(m, c0, c1, c2, c3) &&                                             \
-        (((uint32_t *) m)[1] & 0xffff) == ((c5 << 8) | c4))
-
-#define str7cmp(m, c0, c1, c2, c3, c4, c5, c6)                                 \
-    (str6cmp(m, c0, c1, c2, c3, c4, c5) && (m[6] == c6))
-
-#define str8cmp(m, c0, c1, c2, c3, c4, c5, c6, c7)                             \
-    (str4cmp(m, c0, c1, c2, c3) &&                                             \
-        (((uint32_t *) m)[1] == ((c7 << 24) | (c6 << 16) | (c5 << 8) | c4)))
-
-#define str9cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8)                         \
-    (str8cmp(m, c0, c1, c2, c3, c4, c5, c6, c7) && m[8] == c8)
-
-#define str10cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9)                    \
-    (str8cmp(m, c0, c1, c2, c3, c4, c5, c6, c7) &&                             \
-        (((uint32_t *) m)[2] & 0xffff) == ((c9 << 8) | c8))
-
-#define str11cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10)               \
-    (str10cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9) && (m[10] == c10))
-
-#define str12cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11)          \
-    (str8cmp(m, c0, c1, c2, c3, c4, c5, c6, c7) &&                             \
-        (((uint32_t *) m)[2] == ((c11 << 24) | (c10 << 16) | (c9 << 8) | c8)))
-
-#else // BIG ENDIAN
-
-#define str4cmp(m, c0, c1, c2, c3)                                             \
-    (str3cmp(m, c0, c1, c2) && (m3 == c3))
+    (str3cmp(m, c0, c1, c2) && (m[3] == c3))
 
 #define str5cmp(m, c0, c1, c2, c3, c4)                                         \
     (str4cmp(m, c0, c1, c2, c3) && (m[4] == c4))
@@ -127,9 +91,6 @@ void bstring_free(struct bstring **bstring);
 
 #define str12cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11)          \
     (str11cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10) && m[11] == c11)
-
-#endif // CC_LITTLE_ENDIAN
-
 
 /*
  * Wrapper around common routines for manipulating C character strings
