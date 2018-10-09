255a9a158220513e66e1beca1cee7af5d37c43e2
Yao Yue <yao@twitter.com>
2018-10-08 17:09:20 -0700

    an improved generic string comparison, preserving original little endian version as comment

--- include/cc_bstring.h
+++ include/cc_bstring.h
@@ -66,7 +66,8 @@ void bstring_free(struct bstring **bstring);
     (m[0] == c0 && m[1] == c1 && m[2] == c2)
 
 #define str4cmp(m, c0, c1, c2, c3)                                             \
-    (str3cmp(m, c0, c1, c2) && (m[3] == c3))
+    ((m[0] << 24 | m[1] << 16 | m[2] << 8 | m[3]) ==                           \
+        ((c0 << 24) | (c1 << 16) | (c2 << 8) | c3))
 
 #define str5cmp(m, c0, c1, c2, c3, c4)                                         \
     (str4cmp(m, c0, c1, c2, c3) && (m[4] == c4))
@@ -78,7 +79,9 @@ void bstring_free(struct bstring **bstring);
     (str6cmp(m, c0, c1, c2, c3, c4, c5) && m[6] == c6)
 
 #define str8cmp(m, c0, c1, c2, c3, c4, c5, c6, c7)                             \
-    (str7cmp(m, c0, c1, c2, c3, c4, c5, c6) && m[7] == c7)
+    (str4cmp(m, c0, c1, c2, c3) &&                                             \
+        (m[4] << 24 | m[5] << 16 | m[6] << 8 | m[7]) ==                        \
+        ((c4 << 24) | (c5 << 16) | (c6 << 8) | c7))
 
 #define str9cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8)                         \
     (str8cmp(m, c0, c1, c2, c3, c4, c5, c6, c7) && m[8] == c8)
@@ -90,7 +93,51 @@ void bstring_free(struct bstring **bstring);
     (str10cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9) && m[10] == c10)
 
 #define str12cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11)          \
-    (str11cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10) && m[11] == c11)
+    (str8cmp(m, c0, c1, c2, c3, c4, c5, c6, c7) &&                             \
+        (m[8] << 24 | m[9] << 16 | m[10] << 8 | m[11]) ==                      \
+        ((c8 << 24) | (c9 << 16) | (c10 << 8) | c11))
+
+/* below is a more efficient implementation for little-endian only, it takes
+ * about 50% the cycles compared to the generic implementation above in the
+ * extreme cases (e.g. string length being multiples of 4), however, our
+ * profiling showed that string comparison does not contribute meaningfully to
+ * overall processing cost, both events and hashes are far more notable, and
+ * therefore we can choose the generic implementation until profiling results
+ * indicate otherwise.
+ */
+
+/*
+#define str4cmp(m, c0, c1, c2, c3)                                             \
+    (*(uint32_t *) m == ((c3 << 24) | (c2 << 16) | (c1 << 8) | c0))
+
+#define str5cmp(m, c0, c1, c2, c3, c4)                                         \
+    (str4cmp(m, c0, c1, c2, c3) && (m[4] == c4))
+
+#define str6cmp(m, c0, c1, c2, c3, c4, c5)                                     \
+    (str4cmp(m, c0, c1, c2, c3) &&                                             \
+        (((uint32_t *) m)[1] & 0xffff) == ((c5 << 8) | c4))
+
+#define str7cmp(m, c0, c1, c2, c3, c4, c5, c6)                                 \
+    (str6cmp(m, c0, c1, c2, c3, c4, c5) && (m[6] == c6))
+
+#define str8cmp(m, c0, c1, c2, c3, c4, c5, c6, c7)                             \
+    (str4cmp(m, c0, c1, c2, c3) &&                                             \
+        (((uint32_t *) m)[1] == ((c7 << 24) | (c6 << 16) | (c5 << 8) | c4)))
+
+#define str9cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8)                         \
+    (str8cmp(m, c0, c1, c2, c3, c4, c5, c6, c7) && m[8] == c8)
+
+#define str10cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9)                    \
+    (str8cmp(m, c0, c1, c2, c3, c4, c5, c6, c7) &&                             \
+        (((uint32_t *) m)[2] & 0xffff) == ((c9 << 8) | c8))
+
+#define str11cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10)               \
+    (str10cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9) && (m[10] == c10))
+
+#define str12cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11)          \
+    (str8cmp(m, c0, c1, c2, c3, c4, c5, c6, c7) &&                             \
+        (((uint32_t *) m)[2] == ((c11 << 24) | (c10 << 16) | (c9 << 8) | c8)))
+*/
 
 /*
  * Wrapper around common routines for manipulating C character strings
