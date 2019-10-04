49907e5d0644492d974c1c415fb06a9ead4cb9b3
Sean Lynch <slynch@twitter.com>
2019-10-04 11:48:44 -0700

    Add missing license header to cc_binding

--- rust/cc_binding/src/metric.rs
+++ rust/cc_binding/src/metric.rs
@@ -1,3 +1,18 @@
+// ccommon - a cache common library.
+// Copyright (C) 2019 Twitter, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use std::mem::{align_of, size_of, MaybeUninit};
 use std::os::raw::c_char;
 
@@ -26,6 +41,7 @@ impl metric_anon_union {
 
         (&self.bytes).align_to().1.as_ptr()
     }
+    
     pub unsafe fn as_mut_ptr<T>(&mut self) -> *mut T {
         assert!(align_of::<T>() <= align_of::<Self>());
         assert!(size_of::<T>() <= size_of::<Self>());
