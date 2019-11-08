fecc9868692bb30593f5390d3aadadd6a35ec4f8
Sean Lynch <slynch@twitter.com>
2019-11-08 14:10:48 -0800

    Add more license headers

--- rust/ccommon-backend/tests/parse.rs
+++ rust/ccommon-backend/tests/parse.rs
@@ -1,8 +1,23 @@
-use ccommon_backend::option::*;
-use ccommon_rs::{Options, option::*};
+// ccommon - a cache common library.
+// Copyright (C) 2019 Twitter, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-use std::io::Cursor;
+use ccommon_backend::option::*;
+use ccommon_rs::{option::*, Options};
+
 use std::ffi::CStr;
+use std::io::Cursor;
 
 #[derive(Options)]
 #[repr(C)]
@@ -163,11 +178,13 @@ fn test_parse_string() {
 fn test_parse_comments() {
     let mut options = TestOptions::new();
 
-    let mut cursor = Cursor::new(&br#"
+    let mut cursor = Cursor::new(
+        &br#"
         # Test Comment
         uint_option: 22
         # uint_option: 34
-    "#[..]);
+    "#[..],
+    );
     options.load(&mut cursor).unwrap();
     assert_eq!(options.uint_option.value(), 22);
 }
