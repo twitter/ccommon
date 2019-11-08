8dfe3a1d127b65f98f3b25c1b9236d4b1dd4438f
Sean Lynch <slynch@twitter.com>
2019-11-08 13:44:21 -0800

    Add missing docs

--- rust/ccommon-backend/src/option/default.rs
+++ rust/ccommon-backend/src/option/default.rs
@@ -1,3 +1,18 @@
+// ccommon - a cache common library.
+// Copyright (C) 2019 Twitter, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use cc_binding::{
     _cc_alloc, option, OPTION_TYPE_BOOL, OPTION_TYPE_FPN, OPTION_TYPE_STR, OPTION_TYPE_UINT,
 };
@@ -22,6 +37,7 @@ impl fmt::Display for OutOfMemoryError {
 
 impl std::error::Error for OutOfMemoryError {}
 
+/// Initialize an option to it's default value.
 pub unsafe fn option_default(opt: &mut option) -> Result<(), OutOfMemoryError> {
     use std::mem::MaybeUninit;
 
@@ -60,6 +76,7 @@ pub unsafe fn option_default(opt: &mut option) -> Result<(), OutOfMemoryError> {
     Ok(())
 }
 
+/// Initialize all the options to their default values.
 pub unsafe fn option_load_default(options: &mut [option]) -> Result<(), OutOfMemoryError> {
     for opt in options.iter_mut() {
         option_default(opt)?;
