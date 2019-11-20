df74087e7c892f52e6f93a7e3536e77645308f97
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2019-11-20 15:33:15 -0800

    Address clippy lints in rust code (#234)
    * Address clippy lints within ccommon-derive
    
    * Properly silence clippy warnings within cc_bindings
    
    * Address clippy lints in ccommon-backend
    
    * Address clippy lints in ccommon_rs

--- rust/ccommon_rs/src/option/mod.rs
+++ rust/ccommon_rs/src/option/mod.rs
@@ -14,7 +14,7 @@
 // limitations under the License.
 
 //! Types and methods for dealing with ccommon options.
-//! 
+//!
 //! See the docs on the `Options` macro to see how to create
 //! new options structs.
 
@@ -189,6 +189,7 @@ pub trait OptionExt: Options {
 
     /// Load options from a file.
     #[deprecated(note = "Use load instead - this interface is unsafe")]
+    #[allow(clippy::not_unsafe_ptr_arg_deref)]
     fn load_from_libc_file(&mut self, file: *mut libc::FILE) -> Result<(), crate::Error> {
         use ccommon_backend::compat::CFileRef;
         use std::io::BufReader;
