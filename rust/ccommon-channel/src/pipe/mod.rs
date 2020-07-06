a5c8513a8396fa01aa22c1a1d6b4eb3be7e71d28
Brian Martin <bmartin@twitter.com>
2020-07-06 16:02:14 -0700

    fix copy paste fail

--- rust/ccommon-channel/src/pipe/mod.rs
+++ rust/ccommon-channel/src/pipe/mod.rs
@@ -6,44 +6,32 @@
 use serde::{Deserialize, Serialize};
 
 // constants to define default values
-const TCP_BACKLOG: usize = 128;
-const TCP_POOLSIZE: usize = 0;
+const PIPE_POOLSIZE: usize = 0;
 
 // helper functions
-fn backlog() -> usize {
-    TCP_BACKLOG
-}
-
 fn poolsize() -> usize {
-    TCP_POOLSIZE
+    PIPE_POOLSIZE
 }
 
 // definitions
 #[derive(Debug)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-pub struct TcpConfig {
-	#[cfg_attr(feature = "serde", serde(default = "backlog"))]
-    backlog: usize,
+pub struct PipeConfig {
     #[cfg_attr(feature = "serde", serde(default = "poolsize"))]
     poolsize: usize,
 }
 
 // implementation
-impl TcpConfig {
-    pub fn backlog(&self) -> usize {
-        self.backlog
-    }
-
+impl PipeConfig {
     pub fn poolsize(&self) -> usize {
         self.poolsize
     }
 }
 
 // trait implementations
-impl Default for TcpConfig {
+impl Default for PipeConfig {
     fn default() -> Self {
         Self {
-            backlog: backlog(),
             poolsize: poolsize(),
         }
     }
